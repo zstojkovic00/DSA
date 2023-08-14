bf906177ea333c0563f3346ce9dd8bbd8a7ff2e3
zstojkovic <zeljko.stojkovic@yettle.rs>
2023-08-14 14:58:24 +0200

    Implementation of Red-Black Tree

---
+++ src/Tree/RedBlackTree/RedBlackTree.java
@@ -0,0 +1,164 @@
+package Tree.RedBlackTree;
+
+
+import Tree.AVLTree.AVL;
+import lombok.Data;
+
+public class RedBlackTree {
+
+
+    @Data
+    public class Node {
+        private int value;
+        private int color;
+        private Node left;
+        private Node right;
+        private Node parent;
+
+        public Node(int value, int color) {
+            this.value = value;
+            this.color = color;
+        }
+    }
+
+    private Node root;
+    private static final int BLACK = 0;
+    private static final int RED = 1;
+
+    public RedBlackTree(){
+
+    }
+
+    public boolean isEmpty(){
+        return root == null;
+    }
+
+    public void display() {
+        display(root, "Root Node: ");
+    }
+
+    private void display(Node node, String details) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(details + node.value + " (Color: " + (node.color == BLACK ? "Black" : "Red") + ")");
+
+        display(node.left, "Left child of " + node.getValue() + " : ");
+        display(node.right, "Right child of " + node.getValue() + " : ");
+    }
+
+    private void insert(int value){
+        Node newNode = new Node(value,RED);
+        root = insert(root, newNode);
+        fixInsertion(newNode);
+    }
+
+    private Node insert(Node root, Node newNode){
+        if(root == null){
+            return newNode;
+        }
+        if (newNode.value < root.value) {
+            root.left = insert(root.left, newNode);
+            root.left.parent = root;
+        }
+        else if (newNode.value > root.value) {
+            root.right = insert(root.right, newNode);
+            root.right.parent = root;
+        }
+        return root;
+    }
+
+
+    private void fixInsertion(Node node) {
+        while (node.parent != null && node.parent.color == RED) {
+            Node parent = node.parent;
+            Node grandparent = parent.parent;
+
+            if (parent == grandparent.left) {
+                Node uncle = grandparent.right;
+
+                if (uncle != null && uncle.color == RED) {
+                    parent.color = BLACK;
+                    uncle.color = BLACK;
+                    grandparent.color = RED;
+                    node = grandparent;
+                } else {
+                    if (node == parent.right) {
+                        node = parent;
+                        leftRotate(node);
+                    }
+                    parent.color = BLACK;
+                    grandparent.color = RED;
+                    rightRotate(grandparent);
+                }
+            } else {
+                Node uncle = grandparent.left;
+
+                if (uncle != null && uncle.color == RED) {
+                    parent.color = BLACK;
+                    uncle.color = BLACK;
+                    grandparent.color = RED;
+                    node = grandparent;
+                } else {
+                    if (node == parent.left) {
+                        node = parent;
+                        rightRotate(node);
+                    }
+                    parent.color = BLACK;
+                    grandparent.color = RED;
+                    leftRotate(grandparent);
+                }
+            }
+        }
+        root.color = BLACK;
+    }
+
+    private void leftRotate(Node node) {
+        Node rightChild = node.right;
+        node.right = rightChild.left;
+        if (rightChild.left != null) {
+            rightChild.left.parent = node;
+        }
+        rightChild.parent = node.parent;
+        if (node.parent == null) {
+            root = rightChild;
+        } else if (node == node.parent.left) {
+            node.parent.left = rightChild;
+        } else {
+            node.parent.right = rightChild;
+        }
+        rightChild.left = node;
+        node.parent = rightChild;
+    }
+
+    private void rightRotate(Node node) {
+        Node leftChild = node.left;
+        node.left = leftChild.right;
+        if (leftChild.right != null) {
+            leftChild.right.parent = node;
+        }
+        leftChild.parent = node.parent;
+        if (node.parent == null) {
+            root = leftChild;
+        } else if (node == node.parent.right) {
+            node.parent.right = leftChild;
+        } else {
+            node.parent.left = leftChild;
+        }
+        leftChild.right = node;
+        node.parent = leftChild;
+    }
+
+    public static void main(String[] args) {
+        RedBlackTree tree = new RedBlackTree();
+        tree.insert(8);
+        tree.insert(5);
+        tree.insert(12);
+        tree.insert(18);
+
+        tree.display();
+    }
+
+
+
+}
